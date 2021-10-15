0761cdb93f5a05bb3c6687b6571ff0fbf068fc6c
destiny williams <bishde5v@gmail.com>
2021-10-15 12:33:37 -0500

    continued pratice

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -9,7 +9,7 @@ var arr = [10,20,30,40,50,60]
 //Code Here
 let firstItem = arr[0] 
  console.log(firstItem);
-
+ 
 ////////// PROBLEM 2 //////////
 /*
   Remove the last item from the 'arr' array and store it in a variable
@@ -51,21 +51,47 @@ let evensArr = []
 
 //Code Here
 
+for (let i = 0; i < nums.length; i++){
+  if (nums[i] % 2 === 0){
+    evensArr.push (nums[i])
+  }
+}
+console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var score = 74
+var score = 44
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'.
+   If the variable is a 90 or above, 
+   console log an 'A', 
+   between 80 and 89,
+    console log a 'B', 
+    between 70 and 79, 'C', 
+    between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
 //Code Here
 
+if (score >= 90){
+  console.log("A")
 
+} else if (score <= 89 && score >= 80){
+  console.log("B")
+
+} else if (score <= 79 && score >= 70){
+console.log('C')
+
+} else if (score <= 69 && score >= 60){
+console.log("D")
+
+} else if (score < 60){
+  console.log("F")
+}
 ////////// Intermediate Problems //////////
 
 ////////// PROBLEM 6 //////////
@@ -75,16 +101,25 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Do not edit the code above.
 
 /*
-  Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
+  Create a variable named 'someNum' and set it equal to the
+   fifth value of the 'myFavoriteNumbers' array.
 */
 
 //Code Here
 
+let someNum = myFavoriteNumbers[5] 
+ console.log(someNum)
+
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// Subscripting (accessing values using their index) an array can fail.
+// What happens if you subscript to the 7th element, but there are not 7 elements in the array? 
+//Let's write some code to check for that. 
+// Use an if statement to check the length of the 'myFavoriteNumbers' array.
+// If it's less than 7, log 'There are not enough elements in this array' to the console.
+// If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array.
+// (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
 
