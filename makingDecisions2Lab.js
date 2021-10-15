fe69019f3c3df0e00bb966c16816c0e9d2cd9301
destiny williams <bishde5v@gmail.com>
2021-10-15 13:18:16 -0500

    lunch time update

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -107,7 +107,7 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
-let someNum = myFavoriteNumbers[5] 
+let someNum = myFavoriteNumbers[4] 
  console.log(someNum)
 
 
@@ -118,11 +118,18 @@ let someNum = myFavoriteNumbers[5]
 //Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array.
 // If it's less than 7, log 'There are not enough elements in this array' to the console.
-// If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array.
-// (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// If the length is more than 7, reassign the value of 'someNum' 
+//to the value of the 7th element in the array.
+// (Hint: how can you make sure that your code works for exactly 7 elements?
+// What index do you use to get the 7th element?)
 
 //Code Here
-
+if (myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array")
+} else {
+  someNum = myFavoriteNumbers[6]
+  console.log(someNum)
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -130,10 +137,15 @@ let someNum = myFavoriteNumbers[5]
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. 
+//If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for (var i = 0; i % 3; i++){
+  if(listOfNumbers[i] % 3 === 0){
+    console.log('[i] is divisible by 3')
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
